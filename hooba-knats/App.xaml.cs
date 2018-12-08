30e5136ed5940b1c60bc49351b6413cbbd064ee5
theidanlapid <idanl22@cs.colman.ac.il>
2018-12-08 15:38:57 +0200

    Added key up event

--- hooba-knats/App.xaml.cs
+++ hooba-knats/App.xaml.cs
@@ -1,15 +1,9 @@
-﻿using System.Windows.Input;
-
-namespace hooba_knats
+﻿namespace hooba_knats
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
-        private void Application_OnKeyboardKeyUp(object sender, KeyEventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
